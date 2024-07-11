86f6de8e224d9186d4ad8d812ece77b35f59ae70
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-11 08:04:20 +0530

    DAY 1 OF 30 DAYS OF JS BY ASABENAH

---
+++ day_1/index.js
@@ -0,0 +1,9 @@
+`use strict`;
+// Day1 Of 30 days of JS Challenge by Asabenah
+
+// 1. comments can make code redable.
+/* 2. Comments can make code redable,
+    easy to reuse and 
+            Informative.
+*/
+// 3 This is 30 days of JavaScript Series.
---
+++ day_1/variable.js
@@ -0,0 +1,34 @@
+`use strict`;
+
+const name = "Arpit Mishra";
+let isMarried = false;
+let hasMasters = null;
+let FirstDeployment;
+
+console.log(typeof name);
+console.log(typeof isMarried);
+console.log(typeof hasMasters);
+console.log(typeof FirstDeployment);
+
+let first;
+let second;
+let third;
+let fourth;
+
+let fifth = "Rohit";
+let age = 25;
+let country = "India";
+let hasMarried = true;
+
+
+let firstName = 'Arpit',lastName = 'Mishra',marriedStatus = false,myCountry = 'india';
+console.log(firstName,lastName,isMarried,myCountry);
+let firstName1 = 'Arpit';
+let lastName1 = 'Mishra';
+let marriedStatus1 = false;
+let myCountry1= 'india';
+console.log(firstName1,lastName1,marriedStatus1,myCountry);
+
+let myAge = 25;
+let yourAge = 30;
+console.log('I am ' + myAge + ' Years old! ' +'\n' + 'And You are: ' + yourAge + ' Years old!');
