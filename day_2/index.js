574bf1d51aec978561e3ce6b833376d3fdecd0b9
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-12 11:57:38 +0530

    Level-2 of Day-2 completed

--- day_2/index.js
+++ day_2/index.js
@@ -26,3 +26,29 @@ const bigCompanies = 'Meta,Google,Microsoft,Apple,IBM,Oracle,Amazon';
 // console.log(challenge.match(/a/gi));
 // console.log(`30 days of `.concat('React'));
 // console.log(challenge.repeat(2));
+
+// **************************************************************
+// **************************************************************
+
+// Exercise Level 2  Challenge
+// console.log(`The Quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help another.`);
+// console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.");
+// console.log(+'10' === 10);
+// console.log(Math.round(parseFloat('9.8')) === 10);
+// console.log(`python`.includes(`on`));
+// console.log(`jargon`.includes(`on`));
+// console.log(Math.floor(Math.random() * 101));
+// console.log((Math.floor(Math.random() * 50)) + 50);
+// console.log(Math.floor(Math.random() * 256));
+let str = 'JavaScript';
+let randomIndex = Math.floor(Math.random() * str.length);
+// console.log(str[randomIndex]);
+// console.log(`1 1 1 1 1
+// 2 1 2 4 8
+// 3 1 3 9 27
+// 4 1 4 16 64
+// 5 1 5 25 125`);
+let startIndex = 'You cannot end a sentence with because because because is a conjunction'.indexOf(`because`);
+let endIndex = 'You cannot end a sentence with because because because is a conjunction'.lastIndexOf(`because`);
+// console.log('You cannot end a sentence with because because because is a conjunction'.match(/because/gi));
+// console.log('You cannot end a sentence with because because because is a conjunction'.substring());
