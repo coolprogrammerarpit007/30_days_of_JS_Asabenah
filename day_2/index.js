f0c1442dc57e4569e63c99861c5571fb3fff396e
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-12 11:21:37 +0530

    Exercise Level 1  completed of day 2 of 30 days of JS challenge

---
+++ day_2/index.js
@@ -0,0 +1,28 @@
+`use strict`;
+
+// Level-1 
+
+let challenge = '30 days of JavaScript Challenge';
+// console.log(challenge);
+// console.log(challenge.length);
+// console.log(challenge.toUpperCase());
+// console.log(challenge.toLocaleLowerCase());
+// console.log(challenge.substring(0,2));
+// console.log(challenge.toLowerCase().includes('challenge'));
+// console.log(challenge.split(' '));
+const bigCompanies = 'Meta,Google,Microsoft,Apple,IBM,Oracle,Amazon';
+// console.log(bigCompanies.split(','));
+// console.log(challenge.replace('JavaScript','Python'));
+// console.log(challenge.charAt(15));
+// console.log(challenge.charCodeAt('J'));
+// console.log(challenge.indexOf('a'));
+// console.log(challenge.lastIndexOf('a'));
+// console.log('You cannot end a sentence with because because because is a conjunction'.indexOf('because'));
+// console.log('You cannot end a sentence with because because because is a conjunction'.lastIndexOf('because'));
+// console.log('You cannot end a sentence with because because because is a conjunction'.search('because'));
+// console.log('   30 days of JavaScript '.trim());
+// console.log(challenge.startsWith('31'));
+// console.log(challenge.endsWith('30'));
+// console.log(challenge.match(/a/gi));
+// console.log(`30 days of `.concat('React'));
+// console.log(challenge.repeat(2));
