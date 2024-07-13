c180c4addffd219b752a12ce2b4be55df9df6d56
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-13 07:08:51 +0530

    Day-3 Level 1 Exercises Completed

---
+++ day_3/index.js
@@ -0,0 +1,57 @@
+`use strict`;
+
+// Falsey Values are:- 0,0n,null,undefined,NaN,false,''
+// Exercise Level 1
+
+let firstName = `Arpit`;
+let lastName = `Mishra`;
+let country = `India`;
+let city = `Jaipur`;
+let age = 25;
+let isMarried = false;
+// console.log(typeof firstName);
+// console.log(typeof lastName);
+// console.log(typeof country);
+// console.log(typeof city);
+// console.log(typeof age);
+// console.log(typeof isMarried);
+// console.log(typeof '10' == typeof 10);// false
+// console.log(parseInt(9.8) == 10); // false
+// // Boolean is either true or false
+// console.log(Boolean(1));
+// console.log(Boolean("hgg"));
+// console.log(Boolean({}));
+// console.log(Boolean(0));
+// console.log(Boolean(''));
+// console.log(Boolean(null));
+// console.log(4>3);//true
+// console.log(4>=3);//true
+// console.log(4<3);//false
+// console.log(4<=3);//false
+// console.log(4==4);//true
+// console.log(4===4);//true
+// console.log(4!=4);//false
+// console.log(4!==4);//false
+// console.log(4!='4');//false
+// console.log(4=='4');//true
+// console.log(4==='4');//false
+// console.log(`python`.includes(`on`));
+// console.log(`jargon`.includes(`on`));
+// console.log(4 > 3 && 10 < 12);//true
+// console.log(4 > 3 && 10 > 12);//false
+// console.log(4 > 3 || 10 < 12);//true
+// console.log(4 > 3 || 10 > 12);//true
+// console.log(!(4 > 3));//false
+// console.log(!(4 < 3));//true
+// console.log(!(false));//true
+// console.log(!(4 > 3 && 10 < 12));//false
+// console.log(!(4 > 3 && 10 > 12));//true
+// console.log(!(4 === '4'));//true
+const now = new Date();
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getTime());
