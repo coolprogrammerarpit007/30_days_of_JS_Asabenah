00ecc9b1dce0421b32c83609e889a2595e8e2674
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-14 13:01:08 +0530

    Level 1 of day-4 completed

---
+++ day_4/index.js
@@ -0,0 +1,32 @@
+`use strict`;
+
+// Level 1
+
+// const age = Number(prompt('Enter your age: '));
+// if(age >= 18){
+//     console.log(`Eligible to drive`);
+// }else{
+//     console.log(`Not Eligible to drive! ${18 - age} more years.`);
+// }
+
+// let myAge = Number(prompt('Enter your age: '));
+// let yourAge = 30;
+
+// if(myAge > yourAge){
+//     console.log(`You are: ${myAge - yourAge} older than me!`);
+// }
+// else{
+//     console.log(`You are: ${yourAge - myAge} younger  than me!`);
+// }
+
+let a = 4;
+let b = 3;
+const isGreater = a>b ? `${a} is greater than ${b}` : `${b} is greater than ${a}`;
+// console.log(isGreater);
+
+// let num = Number(prompt(`Enter Number: `));
+// if(num % 2 === 0){
+//     console.log(`${num} is an even number!`);
+// }else{
+//     console.log(`${num} is an odd number!`);
+// }
