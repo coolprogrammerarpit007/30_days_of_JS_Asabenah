692fcf924a81b6561c5806a22eff06e374915b81
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-16 18:12:09 +0530

    level-2 of day-6 completed

--- day_6/index.js
+++ day_6/index.js
@@ -11,7 +11,8 @@ const countries = [
     'Hungary',
     'Ireland',
     'Japan',
-    'Kenya'
+    'Kenya',
+    `Iceland`
   ]
   
   const webTechs = [
@@ -107,9 +108,97 @@ const countries = [
 // }
 // console.log(randArr);
 
-// let str = `0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`;
-// let randStr = ``;
+let str = `0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+let randStr = ``;
 // for(let i=0;i<5;i++){
 //     randStr += str[Math.floor(Math.random() * str.length)]
 // }
 // console.log(randStr);
+
+
+// Level 2
+
+// let randomIndex = Math.floor(Math.random() * 10) + 10;
+// for(let i=5 ; i<= randomIndex; i++){
+//     randStr += str[Math.floor(Math.random() * str.length)]
+// }
+// console.log(randStr);
+// let hexStr = `0123456789ABCDEF`;
+// let hexCode = `#`;
+// for(let i=0;i<=5;i++){
+//     hexCode += hexStr[Math.floor(Math.random() * hexStr.length)]
+// }
+// console.log(hexCode.toLowerCase());
+
+// let red = Math.floor(Math.random() * 256);
+// let green = Math.floor(Math.random() * 256);
+
+// let blue = Math.floor(Math.random() * 256);
+
+// console.log(`rgb(${red},${green},${blue})`);
+
+// const countriesLength = [];
+// for(couuntry of countries){
+//     countriesLength.push(couuntry.length);
+// }
+// console.log(countriesLength);
+let newCountries = [];
+for (let country of countries){
+    newCountries.push([country,country.slice(0,3),country.length]);
+}
+// console.log(newCountries);
+// const landCountry = [];
+// for (let country of countries){
+//     if(country.endsWith(`land`))
+//         landCountry.push(country);
+// }
+// console.log(landCountry);
+const iaCountry = [];
+for (let country of countries){
+    if(country.endsWith(`ia`))
+        iaCountry.push(country);
+}
+// console.log(iaCountry);
+
+// let largestCountry = null;
+// for(let country of countries){
+//     if(largestCountry === null || country.length > largestCountry.length){
+//         largestCountry = country;
+//     }
+// }
+// console.log(largestCountry);
+
+const countries5 = [];
+for(let country of countries){
+    if(country.length === 5){
+        countries5.push(country);
+    }
+}
+// console.log(countries5);
+
+ let largesTech = null;
+for(let tech of webTechs){
+    if(largesTech === null || tech.length > largesTech.length){
+        largesTech = tech;
+    }
+}
+// console.log(largesTech);
+let newTechs = [];
+for (let tech of webTechs){
+    newTechs.push([tech,tech.length]);
+}
+// console.log(newTechs);
+
+let stack = ``;
+for(let str of mernStack){
+    stack += str[0];
+}
+// console.log(stack);
+
+// for (const tech of webTechs) {
+//     console.log(tech);
+// }
+
+// const fruits = ['banana', 'orange', 'mango', 'lemon'] 
+// for(let i=fruits.length-1;i>=0;i--)
+//     console.log(fruits[i]);
