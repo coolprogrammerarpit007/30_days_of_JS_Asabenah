dc2fd8802dda98a3c94badc1ca574c6b14e5d42b
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-21 13:11:46 +0530

    completed level-2 of day-9

--- day_9/index.js
+++ day_9/index.js
@@ -10,7 +10,7 @@ const cube = function(fun,num){
 // console.log(cube(callback,3));
 
 // Exercise Level 1
-const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand',`Fizzy`,`France`]
 const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const products = [
@@ -68,7 +68,7 @@ const countryStr = countries.reduce((acc,country,index,arr) => {
         return `${acc}, ${country}`;
     }
 },"");
-console.log(countryStr);
+// console.log(countryStr);
 
 // console.log(names.some((name) => name.length >= 7));
 // console.log(countries.every(country => country.toLowerCase().includes(`land`)));
@@ -76,3 +76,43 @@ console.log(countryStr);
 // console.log(countries.findIndex(country => country.length === 6));
 // console.log(countries.findIndex(country => country === `Norway`));
 // console.log(countries.find(country => country === `Norway`));
+
+
+// Level 2
+
+const totalPrice = products.map((product) => product.price).filter((price)=>price !== '').reduce((acc,price)=> acc+price,0);
+// console.log(totalPrice);
+const categorizeCountries = countries.filter(country => country.toLowerCase().includes(`land`));
+// console.log(categorizeCountries);
+const countCountry = function(countries){
+    const countryObj = {};
+    countries.forEach(country => {
+        // console.log(country);
+        if(countryObj.hasOwnProperty(`${country[0]}`)){
+            countryObj[country[0]] += 1;
+        }else{
+            countryObj[country[0]] = 1;
+        }
+    });
+    return [countryObj];
+}
+
+// console.log(countCountry(countries));
+
+const maxCountryCount = function(countries){
+    const countryObjArr = countCountry(countries);
+    console.log(countryObjArr);
+    const countryObj = countryObjArr.pop();
+    // console.log(countryObj);
+    let maxAlphabet = ``;
+    let maxCount = null;
+    for(const country in countryObj){
+        if(maxCount === null || maxCount < countryObj[country]){
+            maxCount = countryObj[country];
+            maxAlphabet = country;
+        }
+    }
+    console.log(`${maxAlphabet} comses ${maxCount} times`);
+}
+
+maxCountryCount(countries);
