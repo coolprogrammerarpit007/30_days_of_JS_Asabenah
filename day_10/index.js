45f7c7a39af60ce7d4301422ea5efcaf13ee02cf
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-23 18:47:06 +0530

    level-2 done

--- day_10/index.js
+++ day_10/index.js
@@ -1,9 +1,7 @@
 `use strict`;
 
 // Level 1
-const a = [4, 5, 8, 9];
-const b = [3, 4, 5, 7];
-const countries = ['Finland', 'Sweden', 'Norway'];
+
 const months = ['Jan','Feb','Mar','April','May'];
 
 const setOfNumbers = new Set();
@@ -11,7 +9,7 @@ for(let i=1;i<=10;i++){
     setOfNumbers.add(i)
 }
 
-console.log(setOfNumbers.delete(5));
+// console.log(setOfNumbers.delete(5));
 // setOfNumbers.clear();
 
 // console.log(setOfNumbers);
@@ -28,3 +26,23 @@ const nestedArr = [
 
 const mapOfMonths = new Map(nestedArr);
 // console.log(mapOfMonths);
+
+// Level 2
+
+const a = [4, 5, 8, 9];
+const b = [3, 4, 5, 7];
+// const c = [...a,...b];
+const countries = ['Finland', 'Sweden', 'Norway'];
+
+const A = new Set(a);
+const B = new Set(b);
+// const C = new Set(c);
+// console.log(C);
+
+// to find intersection between sets
+// const c = a.filter(num => B.has(num));
+// to find difference
+const c = a.filter(num => !(B.has(num)));
+// console.log(new Set(c));
+
+
