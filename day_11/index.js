9415458457159c43f7ef91e86e176dc479867c06
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-25 19:42:21 +0530

    completed level 3 of the day 11

--- day_11/index.js
+++ day_11/index.js
@@ -175,3 +175,63 @@ const {width:w,height:h,area:a,perimeter:p} = rectangleObj;
 //         console.log(user);
 //     }
 // })
+
+
+// Level 3
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]];
+
+const [firstName,skills,[,,jsScore,reactScore]] = student;
+// console.log(firstName,skills,jsScore,reactScore);
+
+const students = [
+  ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+  ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+];
+
+const convertArrayToObjects = function([user1,user2]){
+  const [firstName1,skills1,scores1] = user1;
+  const [firstName2,skills2,scores2] = user2;
+  const obj1 = {
+    name:firstName1,
+    skills:skills1,
+    scores:scores1
+  };
+  const obj2 = {
+    name:firstName2,
+    skills:skills2,
+    scores:scores2
+  };
+  return [obj1,obj2]
+}
+
+// console.log(convertArrayToObjects(students));
+
+const student1 = {
+  name: 'David',
+  age: 25,
+  skills: {
+    frontEnd: [
+      { skill: 'HTML', level: 10 },
+      { skill: 'CSS', level: 8 },
+      { skill: 'JS', level: 8 },
+      { skill: 'React', level: 9 }
+    ],
+    backEnd: [
+      { skill: 'Node',level: 7 },
+      { skill: 'GraphQL', level: 8 },
+    ],
+    dataBase:[
+      { skill: 'MongoDB', level: 7.5 },
+    ],
+    dataScience:['Python', 'R', 'D3.js']
+  }
+}
+
+const newStudent = {...student1};
+newStudent.skills.frontEnd.push({'skill':'BootStrap','Level':8});
+newStudent.skills.backEnd.push({'skill':'Express','Level':9});
+newStudent.skills.dataBase.push({'skill':'SQL','Level':8});
+newStudent.skills.dataScience.push('SQL');
+console.log(newStudent);
+// console.log(student1);
+
